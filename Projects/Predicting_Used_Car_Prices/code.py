77992a1f060bc995c02133c49235aa76ea468bf3
Sadaf Saleem <sadafsaleem5815@gmail.com>
2023-03-06 01:51:58 +0500

    updated

--- Projects/Predicting_Used_Car_Prices/code.py
+++ Projects/Predicting_Used_Car_Prices/code.py
@@ -4,13 +4,6 @@ import matplotlib.pyplot as plt
 import matplotlib.style as style
 import seaborn as sns
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import PowerTransformer
-from sklearn.metrics import r2_score,mean_squared_error
-plt.style.use('ggplot')
 
 # loading dataset
 dataset = pd.read_csv('toyota.csv')
@@ -66,97 +59,4 @@ sns.boxplot(data=dataset, x='fuelType',y='price',ax=axes[2]).set(title='The Dist
 for ax in fig.axes:
     plt.sca(ax)
     plt.xticks(rotation=90);
-    
-#Preparing data for modeling
-label_encoder = LabelEncoder()
-dataset['model'] = label_encoder.fit_transform(dataset['model'])
-dataset['transmission'] = label_encoder.fit_transform(dataset['transmission'])
-dataset['fuelType'] = label_encoder.fit_transform(dataset['fuelType'])
-
-feature_cols = ['year','transmission','fuelType','engineSize','tax','model','mileage']
-X = dataset[feature_cols] # Features
-y = dataset['price'] # Target variable
-# define the scaler 
-scaler = PowerTransformer()
-# fit and transform the train set
-X[['year','engineSize','mileage']] = scaler.fit_transform(X[['year','engineSize','mileage']])
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-
-#Linear Regression Model
-lr = LinearRegression()
-lr.fit(X_train, y_train)
-
-#Finding Feature Importance
-resultdict = {}
-for i in range(len(feature_cols)):
-    resultdict[feature_cols[i]] = lr.coef_[i]
-    
-plt.bar(resultdict.keys(),resultdict.values())
-plt.xticks(rotation='vertical')
-plt.title('Feature Importance in Linear Regression Model');
-
-#Decision Tree Regression Model
-tree = DecisionTreeRegressor(max_depth=12,min_samples_split=2,random_state=42)
-tree.fit(X_train,y_train)
-y_pred2 = tree.predict(X_test)
-
-d_r2 = tree.score(X_test, y_test)
-print("Decision Tree Regressor R-squared: {}".format(d_r2))
-
-d_mse = mean_squared_error(y_pred2, y_test)
-d_rmse = np.sqrt(d_mse)
-print("Decision Tree Regressor RMSE: {}".format(d_rmse))
-
-#Finding the best parameter for Decision Tree Regression Model
-train_score = []
-test_score = []
-max_score = 0
-max_pair = (0,0)
-
-for i in range(1,50):
-    tree = DecisionTreeRegressor(max_depth=i,random_state=42)
-    tree.fit(X_train,y_train)
-    y_pred = tree.predict(X_test)
-    train_score.append(tree.score(X_train,y_train))
-    test_score.append(r2_score(y_test,y_pred))
-    test_pair = (i,r2_score(y_test,y_pred))
-    if test_pair[1] > max_pair[1]:
-        max_pair = test_pair
-
-fig, ax = plt.subplots()
-ax.plot(np.arange(1,50), train_score, label = "Training R^2",color='lightcoral')
-ax.plot(np.arange(1,50), test_score, label = "Testing R^2",color='lime')
-print(f'Best max_depth is: {max_pair[0]} \nTesting R^2 is: {max_pair[1]}')
-
-#Finding the feature Importance
-importance = tree.feature_importances_
-
-f_importance = {}
-for i in range(len(feature_cols)):
-     f_importance[feature_cols[i]] = importance[i]
-        
-plt.bar(f_importance.keys(),f_importance.values())
-plt.xticks(rotation='vertical')
-plt.title('Feature Importance in Decision Tree Regression Model');
-
-#Evaluate by Business Criteria
-X_test['Predicted_price'] = np.round(np.exp(y_pred),0)
-X_test['Price'] = np.round(np.exp(y_test),0)
-lr_e = X_test
-
-lr_e['Diff'] = lr_e['Predicted_price'] - lr_e['Price']
-lr_e['Result'] =  lr_e['Diff'] > 1500
-lr_e['Category'] = lr_e['Result'].apply(lambda x: 'Will Not Sell' if x == True else 'Will Sell')
-ax = lr_e['Category'].value_counts(normalize=True).plot.barh()
-ax.bar_label(ax.containers[0])
-ax.set_title('Evaluating Linear Regression Model by KPI');
-X_test['Predicted_price'] = np.round(np.exp(y_pred2),0)
-X_test['Price'] = np.round(np.exp(y_test),0)
-tree_e = X_test
-
-tree_e['Diff'] = tree_e['Predicted_price'] - tree_e['Price']
-tree_e['Result'] =  tree_e['Diff'] > 1500
-tree_e['Category'] = tree_e['Result'].apply(lambda x: 'Will Not Sell' if x == True else 'Will Sell')
-ax = tree_e['Category'].value_counts(normalize=True).plot.barh()
-ax.bar_label(ax.containers[0])
-ax.set_title('Evaluating Decision Tree Regression Model by KPI');
+ 
